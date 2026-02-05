b1b0d4a9e4522ae770a6aae617b6233a0a45e876
Angelina Lozano <angielozarpi@gmail.com>
2026-02-05 04:01:59 -0500

    Add login, register, and dashboard pages - Tailwind styling added to login, register, and dashboard - Added dashboard_view to accounts/views.py as well as updated routes to dashboard - Added path to dashboard in accounts/urls.py - Registered the User model in accounts/admin.py - Switched database in config/settings.py to sqlite for local development purposes

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
 def register_view(request):
@@ -8,7 +9,7 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("dashboard")
+            return redirect("/accounts/dashboard/")
     else:
         form = CustomUserCreationForm()
 
@@ -21,7 +22,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect("dashboard")
+            return redirect("/accounts/dashboard/")
     else:
         form = CustomAuthenticationForm()
 
@@ -31,3 +32,7 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("login")
+
+@login_required
+def dashboard_view(request):
+    return render(request, "accounts/dashboard.html")
