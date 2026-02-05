d83b3d39410764214409134e6c7eaec82af50a27
Yvette Boyd <ev3boyd@gmail.com>
2026-02-05 17:07:03 -0500

    Revert "Add Accounts App, Authentication Pages, and Initial UI Structure"

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -29,12 +28,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'core',
-    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,8 +74,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'end2friends_db',
+        'USER': 'end2friends_user',
+        'PASSWORD': 'newpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -140,5 +140,3 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'accounts.User'
--- core/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-def landing(request):
-    return render(request, "landing.html")
-
-@login_required
-def dashboard(request):
-    return render(request, "dashboard.html")
