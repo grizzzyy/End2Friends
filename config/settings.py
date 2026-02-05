38bade39557277ccd77075a3c95968b6061337dd
ev3 boyd <ev3boyd@gmail.com>
2026-02-05 00:49:18 -0500

    Add landing page, base template, and core URLs

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'accounts',
+    'core',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
