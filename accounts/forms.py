95593eb7eafc38fa914ff2f531c17e2b73a2d373
Angelina Lozano <angielozarpi@gmail.com>
2026-02-24 01:12:58 -0500

    Add profile update forms and view for profile editing

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import User
+from .models import User, UserProfile
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -9,3 +9,21 @@ class CustomUserCreationForm(UserCreationForm):
 
 class CustomAuthenticationForm(AuthenticationForm):
     pass
+
+
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput())
+    bio = forms.CharField(widget=forms.Textarea())
+
+    class Meta:
+        model = UserProfile
+        fields = ["avatar", "bio"]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm, UpdateProfileForm, UpdateUserForm
 
 def register_view(request):
     if request.method == "POST":
@@ -39,4 +39,16 @@ def dashboard_view(request):
 
 @login_required
 def profile_view(request):
-    return render(request, "accounts/profile.html")
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect(to='profile')
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.userprofile)
+
+    return render(request, 'accounts/profile.html', {'user_form': user_form, 'profile_form': profile_form})
