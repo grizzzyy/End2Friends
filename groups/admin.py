390bc887baf11bb092a6fe703b063de943c50bf4
0ddev3 <ev3boyd@gmail.com>
2026-03-11 23:09:26 -0400

    Fix group/models.py imeplementation

--- groups/admin.py
+++ groups/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import StudyChannel, Membership
+from .models import StudyChannel
 
 admin.site.register(StudyChannel)
-admin.site.register(Membership)
