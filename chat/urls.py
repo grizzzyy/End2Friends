23a46b5a2664ef16e9ed3a2041c9c80ec62d3fb5
Angelina Lozano <angielozarpi@gmail.com>
2026-03-25 23:23:09 -0400

    Add inline edit and delete for messages.

--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.inbox, name="inbox"),
     path("dm/<str:username>/", views.start_dm, name="start_dm"),
+    path("delete/<str:room_id>/", views.delete_conversation, name="delete_conversation"),
+    path("messages/<int:message_id>/edit/", views.edit_message, name="edit_message"),
+    path("messages/<int:message_id>/delete/", views.delete_message, name="delete_message"),
     path("<str:room_name>/", views.chat_room, name="chat_room"),
-   path("delete/<str:room_id>/", views.delete_conversation, name="delete_conversation"),
 
 ]
