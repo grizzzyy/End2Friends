97a3bc4688221ed7787985b36ecedb8ce07ab582
0ddev3 <ev3boyd@gmail.com>
2026-04-10 15:06:19 -0400

    Fix Rooms url syntax part 2

--- rooms/urls.py
+++ rooms/urls.py
@@ -7,20 +7,20 @@ urlpatterns = [
     path('', views.room_list, name='room_list'),
     path('create/', views.create_room, name='create_room'),
 
-    # Room detail page (optional, but recommended)
     path('<int:room_id>/', views.room_detail, name='room_detail'),
-
-    # Chat page
     path('<int:room_id>/chat/', views.room_chat, name='room_chat'),
 
-    # Membership + invites
     path('<int:room_id>/join/', views.join_room, name='join_room'),
     path('<int:room_id>/invite/', views.send_invite, name='send_invite'),
+
     path('invites/', views.my_invites, name='my_invites'),
     path('invites/<int:invite_id>/accept/', views.accept_invite, name='accept_invite'),
     path('invites/<int:invite_id>/decline/', views.decline_invite, name='decline_invite'),
-    path('join/<str:code>/', views.join_by_code, name='join_by_code'),
-    path('<int:room_id>/delete/', views.delete_room, name='delete_room'),
-    path('<int:room_id>/channels/create/', views.create_channel, name='create_channel'),
 
+    path('join/<str:code>/', views.join_by_code, name='join_by_code'),
+
+    path('<int:room_id>/delete/', views.delete_room, name='delete_room'),
+
+   
+    path('<int:room_id>/channels/create/', views.create_channel, name='create_channel'),
 ]
