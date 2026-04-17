019bf53d77f08672841872acb885bf34e9b2b83c
0ddev3 <ev3boyd@gmail.com>
2026-04-16 21:49:00 -0400

    Fix: profile picture upload via render, url.py, and settings.py Part 2

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('core.urls')),  # for dashboard
-    path("chat/", include("chat.urls")),  # for the chat app
+    path('', include('core.urls')),
+    path("chat/", include("chat.urls")),
     path('rooms/', include('rooms.urls', namespace='rooms')),
 ]
 
-# Always serve media files — even when DEBUG=False
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
