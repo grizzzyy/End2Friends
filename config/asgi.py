73f57dd09c082c4cc0e8a94a5b2f4f2437af1c1c
0ddev3 <ev3boyd@gmail.com>
2026-04-16 22:45:33 -0400

    remove whitenoise wrapping in asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -3,8 +3,6 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from whitenoise import WhiteNoise
-from django.conf import settings  
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
@@ -12,20 +10,8 @@ django.setup()
 
 import chat.routing
 
-# Base Django ASGI app
-django_asgi_app = get_asgi_application()
-
-# Wrap with WhiteNoise to serve static + media
-django_asgi_app = WhiteNoise(
-    django_asgi_app,
-    root=settings.STATIC_ROOT  # static files
-)
-
-# Add MEDIA files explicitly
-django_asgi_app.add_files(settings.MEDIA_ROOT, prefix="media/")
-
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(chat.routing.websocket_urlpatterns)
     ),
